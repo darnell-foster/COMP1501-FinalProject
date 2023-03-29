78a134d7fd0a130e702fdbc8553ec83e33d9573d
darnell-foster <darnellfoster12@gmail.com>
2023-03-29 14:00:36 -0400

    Added Title Scene and Art Resources

---
+++ Title Scren/TitleScreen.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _on_Start_pressed():
+	# Load the new scene
+	#var new_scene = load(scene_path)
+	
+	# Get the root node of the scene tree
+	#var root = get_tree().get_root()
+	
+	# Replace the current scene with the new one
+	#root.replace_child(root.get_child(0), new_scene.instance())
+	pass;
