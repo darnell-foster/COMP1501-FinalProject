da6992af66a4dd49136b96274d883e59f53783c6
darnell-foster <darnellfoster12@gmail.com>
2023-03-30 20:40:58 -0400

    Fixed all bugs with movment

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,9 +1,5 @@
 extends KinematicBody2D
 
-#------Animation Constants---------
-var idle = false;
-var running = false;
-#----------------------------------
 export var speed = 400
 var screen_size
 var velocity := Vector2.ZERO
@@ -18,7 +14,8 @@ onready var fall_gravity : float =((-2.0 * jump_heght)/(jump_time_to_decent*jump
 onready var sprite = get_node("AnimatedSprite")
 
 func _ready():
-	screen_size = get_viewport_rect().size 
+	screen_size = get_viewport_rect().size
+
 
 func _process(delta):
 	velocity.y += get_gravity() *delta
@@ -26,14 +23,10 @@ func _process(delta):
 		
 	if Input.is_action_pressed("jump") and is_on_floor():
 		jump()
-	
-	# If the sprite is in the air
-	if !is_on_floor(): 
 		sprite.play("Jump")
-		idle = false
-		running = false
 
-		
+	
+	
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
@@ -51,26 +44,22 @@ func get_input_velocity()->float:
 	var horizontal :=0.0
 	
 	if Input.is_action_pressed("move_right"):
-	
-		#if the sprite is not facing the right flip the x scale
-		if(sprite.scale.x < 0): sprite.scale.x = -1* sprite.scale.x
 		horizontal += 1.0
-		if(idle == true):
-			sprite.play("Run")
-			idle = false;
-		
-	if Input.is_action_pressed("move_left"):
-		if(sprite.scale.x > 0):
-			sprite.scale.x = -1* sprite.scale.x;
-		horizontal -= 1.0
-		if(idle == true):
-			sprite.play("Run")
-			idle = false;
-	
-	if(idle == false and velocity.x == 0):
-		sprite.play("Idle")
-		idle = true;
+		if(sprite.scale.x < 0): sprite.scale.x = -1* sprite.scale.x #if the sprite is not facing the right flip the x scale
 
-	
+		
+	elif Input.is_action_pressed("move_left"):
+		horizontal -= 1.0
+		if(sprite.scale.x > 0): sprite.scale.x = -1* sprite.scale.x  #if the sprite is not facing the left flip the x scale
+
+	#if the player is on the floor based on the movment speed picks the sprite animation
+	if is_on_floor():
+		if velocity.x != 0 and sprite.animation != "Run":
+			sprite.play("Run")
+		elif velocity.x == 0 and sprite.animation != "Idle":
+			sprite.play("Idle")
+
+
+		
 	return horizontal
 
