51695a2ecfc705fa5a84b5c9737eead618e05210
darnell-foster <darnellfoster12@gmail.com>
2023-04-04 22:24:38 -0400

    idk

--- Player/Player.gd
+++ Player/Player.gd
@@ -67,3 +67,14 @@ func get_input_velocity()->float:
 		
 	return horizontal
 
+
+
+func _on_Area2D_body_entered(body):
+	get_tree().reload_current_scene()
+
+
+func _on_Spike_body_entered(body):
+	if body is KinematicBody2D:
+		get_tree().reload_current_scene()
+		print("What")
+
